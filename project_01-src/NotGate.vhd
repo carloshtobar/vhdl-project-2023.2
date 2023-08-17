c02a5496cba0406248074dde71a4469511548be0
carloshtobar <32765851+carloshtobar@users.noreply.github.com>
2023-08-17 04:01:34 -0500

    Add files via upload

---
+++ project_01-src/NotGate.vhd
@@ -0,0 +1,27 @@
+-- This file is part of the course "Circuitos Digitales II"
+-- by Carlos Hernan Tobar Arteaga
+-- File name: project_01/NotGate.vhdl
+
+-- Not gate:
+-- f = not x
+
+
+-- Library and packages 
+library	IEEE;
+use IEEE.std_logic_1164.all;
+
+-- Entity (Interface)
+entity NotGate is
+	port(
+		x	:	in 	std_logic;
+		f	:	out	std_logic
+	);
+end entity;
+
+-- Architecture (Implementation)
+architecture arch of NotGate is
+begin
+	F <= x nand x;
+end architecture;	
+
+
